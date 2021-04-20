31807852be35dcee0f8fcb21cda0f1576e1bee43
Ramona Plogmann <ramona.plogmann@gmail.com>
2021-04-20 19:07:10 +0200

    ignore node_modules

--- routes/images.routes.js
+++ routes/images.routes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { upload, uploadSingleImage, deleteSingleImage, deleteAllImagesFromCategory } from "../controllers/images.controllers.js";
+import { upload, uploadSingleImage, deleteSingleImage, deleteAllImagesFromCategory, copyImagesForCategory } from "../controllers/images.controllers.js";
 
 const router = express.Router();
 
 router.post('/addImage/:folder', upload.single('image'), uploadSingleImage);
-router.post('/deleteImage/:folder/:id', upload.single('image'), deleteSingleImage);
-router.post('/deleteAllImagesFromCategory/:category/:id', upload.single('image'), deleteAllImagesFromCategory);
+router.post('/deleteImage', deleteSingleImage);
+router.post('/deleteAllImagesFromCategory/:category/:id', deleteAllImagesFromCategory);
+router.post('/copyImagesFromCategory/:category/:oldId/:newId', copyImagesForCategory);
 
 export default router;
